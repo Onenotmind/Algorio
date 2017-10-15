31b694f45a9c9a146eff621ee45a8ca4156fe780
FionaCY <1162217737@qq.com>
2017-10-15 18:59:49 +0800

    tree

--- tree/traversal.js
+++ tree/traversal.js
@@ -59,11 +59,13 @@ function tree_grad (arr) { // arr为数组，储存着树节点依次数值
 }
 
 
+
+
+
 // 给定数组生成一颗二叉排序树
 
-function tree_binary(arr) {
 
-}
+
 
 /**
  * 《2》树的遍历
@@ -75,22 +77,211 @@ function tree_binary(arr) {
 
 // 先序遍历
 
-function preOrder(tree) {
-
+function preOrder1(tree) {
+	var preListRec = []; //定义保存先序遍历结果的数组
+    var preOrderRec = function(node) {
+    if (node) {//判断二叉树是否为空
+        preListRec.push(node.value); //将结点的值存入数组中
+        preOrderRec(node.left); //递归遍历左子树
+        preOrderRec(node.right); //递归遍历右子树
+    }
+}
+preOrderRec(tree);
+console.log(preListRec);
 }
 
+
+
+function preOrder2(tree) {
+
+}
 // 中序遍历
 
-function order(tree) {
+function inOrder1(tree) {
 
 }
 
+
+
+function inOrder2(tree) {
+
+}
 // 后序遍历
-function lastOrder(tree) {
+function lastOrder1(tree) {
 
 }
 
+
+
+function lastOrder2(tree) {
+
+}
 // 层次遍历
 function level(tree) {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var root = {
+val: 1,
+left: {
+
+val: 2,
+left: {
+  val: 4,
+},
+right:{
+  val:5
+}
+},
+right: {
+
+val: 3,
+left: {
+  val: 6
+},
+right: {
+  val: 7
+}
+}
+}
+
+//1是递归，23是非递归
+
+function DLR1(root){
+if(root!=null){
+    console.log(root.val);
+    DLR1(root.left);
+    DLR1(root.right);
+}
+}
+// DLR1(root)//1,2,4,5,3,6,7
+
+ 
+function DLR2(root){
+       if(!root) {
+    throw new Error('Empty Tree')
+  }
+  var stack = []
+  stack.push(root)
+  while(stack.length !== 0) {
+    root = stack.pop()
+    console.log(root.val)    
+    if(root.right) stack.push(root.right)
+    if(root.left) stack.push(root.left)
+  }
+}
+// DLR2(root)
+
+
+function LDR1(root){
+	if(root!=null){
+		LDR1(root.left);
+        console.log(root.val);
+	    LDR1(root.right);
+	}
+}
+// LDR1(root)//4251637
+
+
+function LDR2(root){
+	if(!root) {
+    throw new Error('Empty Tree')
+  }  
+  var stack = []
+  while(stack.length !== 0 || root) {
+    if(root) {
+      stack.push(root)
+      root = root.left
+    } else {
+      root = stack.pop()
+      console.log(root.val)
+      root = root.right
+    }
+  }
+}
+// LDR2(root)
+
+
+function LRD1(root){
+	if(root!=null){
+		LRD1(root.left);
+		LRD1(root.right);
+		console.log(root.val)
+	}
+}
+// LRD1(root)//4526731
+
+
+function LRD2(root){//(使用一个栈)
+	  if(!root) {
+    throw new Error('Empty Tree')
+  }
+  var stack = []
+  stack.push(root)
+  var tmp = null
+  while(stack.length !== 0) {
+    tmp = stack[stack.length - 1]
+    if(tmp.left && root !== tmp.left && root !== tmp.right) {
+      stack.push(tmp.left)
+    } else if(tmp.right && root !== tmp.right) {
+      stack.push(tmp.right)
+    } else {
+      console.log(stack.pop().val)
+      root = tmp
+    }
+  }
+}
+// LRD2(root);
+
+
+function LRD3 (root) {//(使用两个栈),s1有点像一个临时变量
+  if(root) {
+    var s1 = []
+    var s2 = []
+    s1.push(root)
+    while(s1.length !== 0) {
+      root = s1.pop()
+      s2.push(root)
+      if(root.left) {
+        s1.push(root.left)
+      }
+      if(root.right) {
+        s1.push(root.right)
+      }
+    }
+    while(s2.length !== 0) {
+      console.log(s2.pop().val);
+    }
+  }
+}
+// LRD3(root)
+
+
+function level2(root) {
+  if(!root) {
+    throw new Error('Empty Tree')
+  }
+  var que = []
+  que.push(root)
+  while(que.length !== 0) {
+    root = que.shift()
+    console.log(root.val)
+    if(root.left) que.push(root.left)
+    if(root.right) que.push(root.right)
+  }
+}
+// level2(root)//1234567
+
